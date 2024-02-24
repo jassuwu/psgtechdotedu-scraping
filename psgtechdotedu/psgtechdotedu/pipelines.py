75440ef052c29bfeb7e952445aefafe09b366dab
jassuwu <karthickprnv@gmail.com>
2024-02-24 09:53:09 +0530

    update td_matrix.csv store location

--- psgtechdotedu/psgtechdotedu/pipelines.py
+++ psgtechdotedu/psgtechdotedu/pipelines.py
@@ -14,25 +14,17 @@ class TfidfPipeline(object):
         self.urls = []
 
     def process_item(self, item, spider):
-        # Add the document text to the list of documents
         joint_document = ' '.join(item['text'])
         self.documents.append(joint_document)
-        # Add the url to the list of urls
         self.urls.append(item['url'])
         item['text'] = joint_document
         return item
 
     def close_spider(self, spider):
-        # Calculate the TD matrix
         vectorizer = TfidfVectorizer()
         td_matrix = vectorizer.fit_transform(self.documents)
 
-        # Convert the TD matrix to a DataFrame
         df = pd.DataFrame(td_matrix.T.toarray(), index=vectorizer.get_feature_names_out(), columns=self.urls)
-
-        # Sort the DataFrame columns in alphabetical order
         df = df.sort_index(axis=1)
-
-        # Write the DataFrame to a CSV file
-        df.to_csv('../data/td_matrix.csv')
+        df.to_csv('data/td_matrix.csv')
 
